06ba8b46966d6940b20161b5596237ea9af326f1
David Polynar <Polynar.david@gmail.com>
2019-10-04 12:00:38 +0200

    Initial commit

---
+++ feladat1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace feladat1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
